c6a705bdcb2b293fd236bbe5f3ccc2ba5fa4419b
cgh0-0 <marketcarrot280@gmail.com>
2026-03-28 09:16:30 +0900

    count 변수 관리가 중점

--- Implementation/1475번/1475번/1475.cpp
+++ Implementation/1475번/1475번/1475.cpp
@@ -9,20 +9,23 @@ int main()
 	for (int i = 0; i < input.size(); ++i)
 	{
 		arr[input[i] - '1'] += 1;
+		
 	}
 	for (int i = 1; i < 10; ++i)
 	{
-		if (i==6 && i == 9)
+		if (i==6 ||i == 9)
 		{
-
-		}
-		else
-		{
-			if (arr[i]>1)
+			if (arr[i] % 2 != 0)
 			{
-				count += arr[i];
+				count = count - (arr[i] / 2) +1;
 			}
+			else if (arr[i] % 2 == 0)
+			{
+				count -= arr[i] / 2;
+			}
+			continue;
 		}
+		if(arr[i]>1)
 	}
 
 
