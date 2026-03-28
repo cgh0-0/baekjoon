c9cb884d7d1abfc815cefa47332fa71d3e92b2bc
cgh0-0 <marketcarrot280@gmail.com>
2026-03-28 13:07:05 +0900

    시각화 필요

--- Implementation/1475번/1475번/1475.cpp
+++ Implementation/1475번/1475번/1475.cpp
@@ -4,33 +4,49 @@ int main()
 {
 	std::string input;
 	std::cin >> input;
-	int arr[1000001][10] = {0};
+	int arr[9] = {0};
 	int count = 0;
 	for (int i = 0; i < input.size(); ++i)
 	{
-		arr[][input[i] - '1'] += 1;
+		arr[input[i] - '1'] += 1;
 		
 	}
-	for (int i = 1; i < 10; ++i)
+	int max = 0;
+	for (int i = 0; i < 10; ++i)
 	{
-		if (arr[i] > 1)
+		
+		if(max<arr[i])
 		{
-
+			max = arr[i];
 		}
-		if (i==6 ||i == 9)
+		else if ((i == 5 || i == 8)&&max<arr[i])
 		{
 			if (arr[i] % 2 != 0)
 			{
-				count = count - (arr[i] / 2) +1;
+					max = (arr[i] / 2) +1;
 			}
 			else if (arr[i] % 2 == 0)
 			{
-				count -= arr[i] / 2;
+
+					max = arr[i] / 2;
 			}
-			continue;
+			else if (arr[i] == 1)
+			{
+				if (max < 1)
+				{
+					max = 1;
+				}
+				
+			}
+				
 		}
-		if(arr[i]>1)
+
+			
+		
+		
+		
 	}
+	std::cout << max;
 
 
 
