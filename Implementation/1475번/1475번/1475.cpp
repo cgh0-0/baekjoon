f6692f2f2d58677d3faeb8d3f9c6539819d9b2b2
cgh0-0 <marketcarrot280@gmail.com>
2026-03-28 15:21:28 +0900

    여긴 나도 내가 뭘 수정한건지 모르겠음

--- Implementation/1475번/1475번/1475.cpp
+++ Implementation/1475번/1475번/1475.cpp
@@ -4,11 +4,11 @@ int main()
 {
 	std::string input;
 	std::cin >> input;
-	int arr[9] = {0};
+	int arr[10] = {0};
 	int count = 0;
 	for (int i = 0; i < input.size(); ++i)
 	{
-		arr[input[i] - '1'] += 1;
+		arr[input[i] - '0'] += 1;
 		
 	}
 	int max = 0;
@@ -17,27 +17,29 @@ int main()
 		
 		if(max<arr[i])
 		{
-			max = arr[i];
-		}
-		else if ((i == 5 || i == 8)&&max<arr[i])
-		{
-			if (arr[i] % 2 != 0)
+
+			if (i == 6 || i == 9)
 			{
-					max = (arr[i] / 2) +1;
-			}
-			else if (arr[i] % 2 == 0)
-			{
+				if (arr[i] % 2 != 0)
+				{
+					max = (arr[i] / 2) + 1;
+				}
+				else if (arr[i] % 2 == 0)
+				{
 
 					max = arr[i] / 2;
-			}
-			else if (arr[i] == 1)
-			{
-				if (max < 1)
-				{
-					max = 1;
 				}
-				
-			}
+				else if (arr[i] == 1)
+				{
+					if (max < 1)
+					{
+						max = 1;
+					}
+					
+				}
+				continue;
+			}	
+		 max = arr[i];
 				
 		}
 
