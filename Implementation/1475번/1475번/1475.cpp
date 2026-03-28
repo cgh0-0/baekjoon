7dfd9ac3fff882778c0e568dcff8db920849a7b9
cgh0-0 <marketcarrot280@gmail.com>
2026-03-28 11:09:09 +0900

    이차원배열로 문제 전환

--- Implementation/1475번/1475번/1475.cpp
+++ Implementation/1475번/1475번/1475.cpp
@@ -4,15 +4,19 @@ int main()
 {
 	std::string input;
 	std::cin >> input;
-	int arr[10] = { 0 };
+	int arr[1000001][10] = {0};
 	int count = 0;
 	for (int i = 0; i < input.size(); ++i)
 	{
-		arr[input[i] - '1'] += 1;
+		arr[][input[i] - '1'] += 1;
 		
 	}
 	for (int i = 1; i < 10; ++i)
 	{
+		if (arr[i] > 1)
+		{
+
+		}
 		if (i==6 ||i == 9)
 		{
 			if (arr[i] % 2 != 0)
