23ef1b0ad2d3b5dedc3fe3da23901e0d5996d50b
cgh0-0 <marketcarrot280@gmail.com>
2026-03-30 13:29:25 +0900

    합쳐서 100이 되는 두 수를 구하기:실패

--- 백지복습/백지 복습/백지 복습/함수 구현.cpp
+++ 백지복습/백지 복습/백지 복습/함수 구현.cpp
@@ -1,56 +1,30 @@
-#include<iostream>
-using namespace std;
-
-void insert(int idx, int num, int arr[], int& len){
-
-	for (int i = len; idx <= i; --i)
+﻿#include<iostream>
+//1강 문제 2 합이 100인 서로 ㅏㄷ른 위치의 두 원소가 존재하는가
+//다른 배열을 만들어서 대응하는 수가 있는지 확인하는걸 생각못함
+int fun2(int arr[], int len)
+{
+	int exist[101];
+	for (int i = 0; i < len; ++i)
 	{
-		arr[i + 1] = arr[i];
+		if (exist[100-arr[i]]==1)
+		{
+			return 1;
+		}
+		exist[arr[i]] = 1;
 	}
-	arr[idx] = num;
-	++len;
+	return 0;
 }
+int main()
+{
+	int arr[3] = { 1,52,48 };
+	
 
-void erase(int idx, int arr[], int& len){
-  
-	for (int i = idx; i < len; ++i)
-	{
-		arr[i] = arr[i + 1];
+	int arr2[2] = { 50,42 };
 
-	}
-	--len;
-}
+	int arr4[4] = { 4,13,63,87 };
 
-void printArr(int arr[], int& len){
-  for(int i = 0; i < len; i++) cout << arr[i] << ' ';
-  cout << "\n\n";
-}
-
-void insert_test(){
-  cout << "***** insert_test *****\n";
-  int arr[10] = {10, 20, 30};
-  int len = 3;
-  insert(3, 40, arr, len); // 10 20 30 40
-  printArr(arr, len);
-  insert(1, 50, arr, len); // 10 50 20 30 40
-  printArr(arr, len);
-  insert(0, 15, arr, len); // 15 10 50 20 30 40
-  printArr(arr, len);
-}
-
-void erase_test(){
-  cout << "***** erase_test *****\n";
-  int arr[10] = {10, 50, 40, 30, 70, 20};
-  int len = 6;
-  erase(4, arr, len); // 10 50 40 30 20
-  printArr(arr, len);
-  erase(1, arr, len); // 10 40 30 20
-  printArr(arr, len);
-  erase(3, arr, len); // 10 40 30
-  printArr(arr, len);
-}
-
-int main(void) {
-  insert_test();
-  erase_test();
+	int a = fun2(arr, 3);
+	int b = fun2(arr2, 2);
+	int c = fun2(arr4, 4);
+	std::cout << a << '\n' << b << '\n' << c;
 }
