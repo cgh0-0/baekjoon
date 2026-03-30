c6277b8e17fce19944fe23925ba1cab7a7bd2109
cgh0-0 <marketcarrot280@gmail.com>
2026-03-30 14:20:19 +0900

    strfry 백지복습 1회차 실패

--- 백지복습/백지 복습/백지 복습/함수 구현.cpp
+++ 백지복습/백지 복습/백지 복습/함수 구현.cpp
@@ -1,30 +1,41 @@
 ﻿#include<iostream>
-//1강 문제 2 합이 100인 서로 ㅏㄷ른 위치의 두 원소가 존재하는가
-//다른 배열을 만들어서 대응하는 수가 있는지 확인하는걸 생각못함
-int fun2(int arr[], int len)
-{
-	int exist[101];
-	for (int i = 0; i < len; ++i)
-	{
-		if (exist[100-arr[i]]==1)
-		{
-			return 1;
-		}
-		exist[arr[i]] = 1;
-	}
-	return 0;
-}
+#include<string>
 int main()
 {
-	int arr[3] = { 1,52,48 };
+	int T;
+	std::cin >> T;
 	
+	for(int j=0;j<T;++j)
+	{
+		int arr[26] = { 0 };
+		int karr[26] = { 0 };
+		std::string str;
+		std::string estr;
+		std::cin >> str;
+		std::cin >> estr;
+		if (str.size() != estr.size())
+		{
+			std::cout << "Impossible" << '\n';
+			continue;
+		}
+		for (int i = 0; i < str.size(); ++i)
+		{
+			arr[str[i] - 'a'] += 1;
+		}
+		for (int i = 0; i < estr.size(); ++i)
+		{
+			karr[estr[i] - 'a'] += 1;
+		}
+		for (int i = 0; i < estr.size(); ++i)
+		{
+			if (arr[i] != karr[i])
+			{
+				std::cout << "Impossible" << '\n';
+				continue;
+			}
+			
+		}
+		std::cout << "Possible" << '\n';
+	}
 
-	int arr2[2] = { 50,42 };
-
-	int arr4[4] = { 4,13,63,87 };
-
-	int a = fun2(arr, 3);
-	int b = fun2(arr2, 2);
-	int c = fun2(arr4, 4);
-	std::cout << a << '\n' << b << '\n' << c;
 }
