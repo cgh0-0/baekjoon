db22e1aff3ed8ac0724b7893e117c67932acf508
cgh0-0 <marketcarrot280@gmail.com>
2026-04-15 11:52:00 +0900

    요세푸스 문제 list함수버전

--- 백지복습/백지 복습/백지 복습/함수 구현.cpp
+++ 백지복습/백지 복습/백지 복습/함수 구현.cpp
@@ -1,1 +1,37 @@
-﻿
+﻿#include<iostream>
+#include<list>
+int main()
+{
+	std::list<int> one;
+	std::list<int>two;
+	int N, K;
+	std::cin >> N >> K;
+	for (int i = 1; i <= N; ++i)
+	{
+		one.push_back(i);
+	}
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < K - 1; ++j)
+		{
+			int tmp = one.front();
+			one.push_back(tmp);
+			one.pop_front();
+		}
+		two.push_back(one.front());
+		one.pop_front();
+	}
+
+	std::list<int>::iterator iter = two.begin();
+	std::cout << "<";
+	for (iter; iter != two.end(); ++iter)
+	{
+		std::cout << *iter;
+		if (*iter != two.back())
+		{
+			std::cout << ", ";
+		}
+	}
+	std::cout << ">";
+
+}
